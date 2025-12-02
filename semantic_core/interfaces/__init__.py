7bc3d42519d812a7e560667294447f934a2ca5e1
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-12-02 11:44:48 +0500

    feat: Добавлен интерфейс DocumentParser и структура ParsingSegment - Создан interfaces/parser.py с протоколом DocumentParser - Определена dataclass ParsingSegment как промежуточная структура - ParsingSegment содержит: content, segment_type, language, headers, start_line, end_line - Экспортированы DocumentParser и ParsingSegment в interfaces/__init__.py - Подготовлена основа для реализации MarkdownNodeParser

--- semantic_core/interfaces/__init__.py
+++ semantic_core/interfaces/__init__.py
@@ -9,16 +9,23 @@
         Абстрактный интерфейс для нарезки текста.
     BaseContextStrategy
         Абстрактный интерфейс для формирования контекста.
+    DocumentParser
+        Протокол для парсеров документов.
+    ParsingSegment
+        Промежуточная структура между парсингом и чанкингом.
 """
 
 from semantic_core.interfaces.embedder import BaseEmbedder
 from semantic_core.interfaces.vector_store import BaseVectorStore
 from semantic_core.interfaces.splitter import BaseSplitter
 from semantic_core.interfaces.context import BaseContextStrategy
+from semantic_core.interfaces.parser import DocumentParser, ParsingSegment
 
 __all__ = [
     "BaseEmbedder",
     "BaseVectorStore",
     "BaseSplitter",
     "BaseContextStrategy",
+    "DocumentParser",
+    "ParsingSegment",
 ]
